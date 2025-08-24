70267707a485ae9aeb3f404a0dbc905928b63d4d
Yahya Aboabdo <yoyo.aboabdo2016@gmail.com>
2025-08-24 20:10:05 +0300

    Backend task 0001

---
+++ 0001.py/0001.py
@@ -0,0 +1,10 @@
+first_name = "Yahya"
+last_name = "Abaobdo"
+GPA = 4.2
+date_of_birth = "7 Apr 2006"
+age = 19
+print('=====User info=====')
+print('Name: '+first_name+' '+last_name)
+print('GPA: '+str(GPA))
+print('Date of birth: '+date_of_birth)
+print('Age: '+str(age))
